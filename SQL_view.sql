8518f03238d354253ced327f3ddb939edfd2f7fc
Arne Elias Tidemand Ruud <67323034+EliasTRuud@users.noreply.github.com>
2023-09-28 00:02:48 +0200

    Added uke 6

---
+++ SQL_view.sql
@@ -0,0 +1,32 @@
+/*
+I SQL er en VIEW en virtuell tabell som ikke inneholder data, men 
+representerer et utvalg av data fra en eller flere tabeller i databasen. En VIEW er forhåndsdefinert spørring (query) som kan utføres for å hente ut informasjon fra de underliggende tabellene.
+
+En VIEW kan brukes til å organisere og presentere data på en mer brukervennlig måte. 
+Den kan også brukes til å filtrere eller formatere dataene før de vises for brukeren. 
+En VIEW kan også forenkle komplekse spørringer ved å kombinere data fra flere tabeller 
+og presentere det som en enkelt tabell.
+*/;
+
+Her er noen eksempler på hvordan VIEW brukes i SQL:
+
+1. Forenkling av spørringer: Dersom man har komplekse spørringer som involverer flere 
+   tabeller, kan man lage et VIEW som kombinerer dataene fra disse tabellene. Deretter kan man bruke VIEW-et i stedet for å skrive den komplekse spørringen flere ganger.
+
+2. Organisering av data: Ved å lage et VIEW som bare viser de kolonnene og radene man er 
+   interessert i, kan man forenkle presentasjonen av dataene. Dette kan være spesielt nyttig for brukere som ikke har teknisk kunnskap om databaser.
+
+3. Sikkerhet: Dersom man ønsker å beskytte sensitiv informasjon fra uautorisert tilgang, kan 
+   man lage et VIEW som bare inkluderer data som visse brukere skal ha tilgang til. 
+   Deretter kan man gi disse brukerne rettighetene til å kun se VIEW-et og ikke hele tabellen.
+
+4. Aggregering av data: Dersom man ønsker å se oppsummeringsinformasjon om dataene i 
+   tabellen, kan man lage et VIEW som gjør dette. VIEW-et kan for eksempel vise totalt 
+   antall rader for en bestemt kategori eller gjennomsnittet av en bestemt kolonne.
+
+5. Formattering av data: Med VIEW kan man formatere dataene for enkel presentasjon eller 
+   analyse. Eksempelvis kan man bruke VIEW til å formatere datoer eller numeriske verdier 
+   på en spesiell måte, og på den måten gjøre det enklere for brukerne å lese og tolke dataene.;
+
+
+
